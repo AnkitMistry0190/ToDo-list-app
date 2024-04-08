eb0a63ca91c1fb47b514bef636af381d98d49e9a
AnkitMistry0190 <153710296+AnkitMistry0190@users.noreply.github.com>
2024-04-08 18:40:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask() {
+  if (inputbox.value === "") {
+    alert("Please type some text before adding");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputbox.value = "";
+  savedata();
+}
+
+addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    savedata();
+  }
+});
+function savedata() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showtask() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
